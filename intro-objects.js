4ce5f9e54fa1eb48eca34e3ba6c5b5c8e8b196af
devyn holman <Devyn@devyns-MacBook-Pro.local>
2016-02-18 16:16:50 -0600

    completed homework

--- intro-objects.js
+++ intro-objects.js
@@ -33,6 +33,31 @@ console.assert(getFullNames(customers)[1] === "John Smith")
 
 var text = "It’s obviously not the case, but T’Challa—the Black Panther and mythical ruler of Wakanda—has always struck as the product of the black nationalist dream, a walking revocation of white supremacist myth. T’Challa isn’t just a superhero in the physical sense, he is one of the smartest people in the world, ruling the most advanced civilization on the planet. Wakanda’s status as ever-independent seems to eerily parallel Ethiopia’s history as well as its place in the broader black imagination. Maybe it’s only me, but I can’t read Jason Aaron’s superb “See Wakanda And Die” and not think of Adowa.\
 Comic book creators, like all story-tellers, get great mileage out of myth and history. But given the society we live in, some people’s myths are privileged over others. Some of that is changing, no doubt. In the more recent incarnations of T’Challa you can see Christopher Priest invoking the language of the Hausa or Reginald Hudlin employing the legacy of colonialism. These were shrewd artistic decisions, rooted in the fact that anyone writing Black Panther enjoys an immediate, if paradoxical, advantage: the black diaspora is terra incognita for much of the world. What does the broader world really know of Adowa? Of Nanny and Cudjoe? Of the Maji-Maji rebellion? Of Legba and Oshun?  Of Shine? Of High John The Conqueror? T’Challa’s writers have always enjoyed access to a rich and under-utilized pool of allusion and invocation."
+// turn text string into array of words
+//identify the words in the array
+//count them
+
+var getCounts = function(inputString){
+
+    var arrayOfWords = inputString.toLowerCase().split(' ') //must lowercase string before splitting it
+
+    for(var i=0; i < arrayOfWords.length; i++){
+
+        var objectForWords = {}
+        var word = arrayOfWords[i]
+
+
+        if (objectForWords.hasOwnProperty(word) === true) {
+
+            objectForWords[word] += 1
+        } else {
+
+            objectForWords[word] = 1 
+        }
+    }
+        return objectForWords
+}
+
 
 var wordFrequencyObject = getCounts(text)
 
@@ -51,8 +76,30 @@ console.assert(wordFrequencyObject.black === 5)
 // The dog constructor (which is what this is) should take a name input, and the
 // dog should receive the assigned name.
 
+var generateDog = function(inputName){
+  
+    this.name = inputName,
+    this.legs = 4,
+    // this.weight = inputWeight
+    // this.color = inputColor
 
-var dog = generateDog('rex')
+    this.speak = function(inputString){
+        var dogSpeak = inputString.split(' ')
+
+        for(i=0; i < dogSpeak.length; i++){
+
+            dogSpeak[i] = 'r' + dogSpeak[i].substr(1)
+            
+            console.log(dogSpeak)
+           
+        }
+        return dogSpeak.join(' ')
+    } 
+}
+
+//google .substring & .slice
+var dog = new generateDog('rex')
+console.log(dog.name)
 
 console.assert(dog.legs === 4)
 console.assert(dog.speak('i love you') === 'r rove rou')
@@ -74,17 +121,61 @@ console.assert(dog.name === 'carl')
 
 // tests
 // ---
+
+var pluck = function(inputArray, prop){
+    
+    var namesArray = []
+
+    for(i=0; i < inputArray.length; i++){
+        
+        var nameObject = inputArray[i]
+
+        namesArray.push(nameObject[prop])
+    }
+
+    log(namesArray)
+
+    return namesArray
+}
+
+
 var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}]
 
 console.assert(pluck(stooges, 'name')[0] === 'moe')
 console.assert(pluck(stooges, 'age')[2] === 60)
 
 
-// Part 5
 
+// Part 5
+ 
 // Write a function reverseObjects that takes an array of objects as input
 // and returns an array of those same objects, but with the properties and 
 // values reversed.
+
+var reverseObjects = function(arrayOfObjects) {
+
+    var reverseArray = []
+
+    var newObj = {}
+
+    for(var i=0; i < arrayOfObjects.length; i++){
+
+        var singleObject = arrayOfObjects[i]
+        
+        for(var prop in singleObject) {
+
+            if(singleObject.hasOwnProperty(prop)){
+
+                newObj[singleObject[prop]] = prop;
+            }
+
+        }
+        reverseArray.push(newObj)
+
+    }
+
+  return reverseArray
+}
 
 
 
@@ -93,9 +184,9 @@ var users = [{obama: 'president@gmail.com',hobby: 'basketball'},{trump: 'america
 
 var flippedUsers = reverseObjects(users)
 
-console.assert(flippedUsers[0]['president@gmail.com'] === 'obama')
-console.assert(flippedUsers[1]['americamoneywin@yahoo.com'] === 'trump')
-console.assert(flippedUsers[1].dealmaking === 'hobby')
+log(flippedUsers[0]['president@gmail.com'] === 'obama')
+log(flippedUsers[1]['americamoneywin@yahoo.com'] === 'trump')
+log(flippedUsers[1].dealmaking === 'hobby')
 
 // Part 6
 
@@ -121,7 +212,7 @@ console.assert(planetTom.name === 'Tom')
 // VVV caution: hints below VVV
 
 console.assert(planetTom['name'] === 'Tom')
-console.assert(planetTom.prop === undefined)
+console.assert(planetTom['prop'] === undefined)
 console.assert(planetTom['prop'] === undefined)
 
 // What did you learn about the difference between
@@ -134,6 +225,30 @@ console.assert(planetTom['prop'] === undefined)
 // a properties object. It should return a new list containing only those
 // objects that meet the key-value conditions.
 // -----------
+var meetsCriteria = function(obj, criteriaObject){
+    for (var property in criteriaObject){
+        if (criteriaObject[property] !== obj[property])
+            return false
+        }
+    }
+
+    return true
+}
+
+var where = function(objArray, criteria){
+
+    var newArrayOfObjects = []
+
+    for(var i=0; i < arrayOfObjects.length; i++){
+        var obj = arrayOfObjects[i]
+
+        if(meetsCriteria(obj,criteria)){
+            newArrayOfObjects.push(obj)
+        }
+    }
+
+        return newArrayOfObjects
+}
 
 
 var plays = [
